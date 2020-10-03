f848792eb84feaa2632eb35d524de0cf9cc77696
kozzzz-07 <kozzzz-07@noreply>
2020-10-03 13:25:04 +0900

    main-nav 高さ調整

--- src/app/shared/components/main-nav/main-nav.component.ts
+++ src/app/shared/components/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-main-nav',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavComponent implements OnInit {
 
+  screenHeight = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getScreenHeightSize();
   }
 
+  // TODO:Directve化 (パフォーマンスも悪いので本来であればrxjsとかでやる)
+  @HostListener('window:resize')
+  getScreenHeightSize(): void {
+    const innerHeight = window.innerHeight;
+    if (this.screenHeight !== innerHeight) {
+      this.screenHeight = window.innerHeight;
+    }
+  }
 }
