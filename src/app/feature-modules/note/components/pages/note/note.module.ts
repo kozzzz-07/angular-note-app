bf73f7777dbdc23da39fd8c4ccfe31edfe4f3403
kozzzz-07 <kozzzz-07@noreply>
2020-10-08 01:12:04 +0900

    不要なコンポーネント削除

--- src/app/feature-modules/note/components/pages/note/note.module.ts
+++ src/app/feature-modules/note/components/pages/note/note.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { NoteRoutingModule } from './note-routing.module';
-import { NoteDetailComponent as NoteDetailContainerComponent } from '../../containers/note-detail/note-detail.component';
 import { NoteDetailComponent } from '../../presentations/note-detail/note-detail.component';
 
 @NgModule({
-  declarations: [NoteDetailComponent, NoteDetailContainerComponent],
+  declarations: [NoteDetailComponent],
   imports: [CommonModule, NoteRoutingModule, FormsModule],
 })
 export class NoteModule {}
--- src/app/feature-modules/note/note.module.ts
+++ src/app/feature-modules/note/note.module.ts
@@ -5,7 +5,6 @@ import { CommonModule } from '@angular/common';
 import { NoteRoutingModule } from './note-routing.module';
 import { NotePageComponent } from './components/pages/note/note.page'; // 名前のバッティングには命名規則で回避するか、モジュールに別名をつけることで回避できる
 import { NoteListComponent as NoteListContainerComponent } from './components/containers/note-list/note-list.component';
-import { NoteDetailComponent as NoteDetailContainerComponent } from './components/containers/note-detail/note-detail.component';
 import { NoteListComponent } from './components/presentations/note-list/note-list.component';
 import { NoteDetailComponent } from './components/presentations/note-detail/note-detail.component';
 import { NoteListItemComponent } from './components/presentations/note-list-item/note-list-item.component';
@@ -15,7 +14,6 @@ import { FormsModule } from '@angular/forms';
   declarations: [
     NotePageComponent,
     NoteListContainerComponent,
-    NoteDetailContainerComponent,
     NoteListComponent,
     NoteDetailComponent,
     NoteListItemComponent,
