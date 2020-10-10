f5b81c51c4365ad10266a68191870e8485e8e104
kozzzz-07 <kozzzz-07@noreply>
2020-10-10 23:10:10 +0900

    fix #1

--- src/app/feature-modules/note/components/presentations/note-list-item/note-list-item.component.ts
+++ src/app/feature-modules/note/components/presentations/note-list-item/note-list-item.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+} from '@angular/core';
 import { NoteID, Note } from 'src/app/models/note/note.model';
 
 @Component({
