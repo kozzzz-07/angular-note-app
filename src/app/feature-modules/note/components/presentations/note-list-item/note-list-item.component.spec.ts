649962571f78095b7a30005270eca0577c1da662
kozzzz-07 <kozzzz-07@noreply>
2020-12-19 15:25:56 +0900

    testing NoteListItemComponent

--- src/app/feature-modules/note/components/presentations/note-list-item/note-list-item.component.spec.ts
+++ src/app/feature-modules/note/components/presentations/note-list-item/note-list-item.component.spec.ts
@@ -22,4 +22,45 @@ describe('NoteListItemComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('titleに"title"が表示されること', () => {
+    component.title = 'title';
+    fixture.detectChanges();
+    const document: HTMLElement = fixture.nativeElement;
+    const titleEl = document.querySelector('.title');
+    expect(titleEl?.textContent).toEqual('title');
+  });
+
+  it('titleに"Untitled"が表示されること', () => {
+    component.title = '';
+    fixture.detectChanges();
+    const document: HTMLElement = fixture.nativeElement;
+    const titleEl = document.querySelector('.title');
+    expect(titleEl?.textContent).toEqual('Untitled');
+  });
+
+  it('dateに作成日が"yy/MM/dd"形式で表示されること', () => {
+    component.createAt = new Date(2020, 10, 20).toISOString();
+    fixture.detectChanges();
+    const document: HTMLElement = fixture.nativeElement;
+    const titleEl = document.querySelector('.date');
+    expect(titleEl?.textContent).toEqual('20/11/20');
+  });
+
+  it('dateに更新日が"yy/MM/dd"形式で表示されること', () => {
+    component.createAt = new Date(2020, 10, 20).toISOString();
+    component.updateAt = new Date(2022, 1, 20).toISOString();
+    fixture.detectChanges();
+    const document: HTMLElement = fixture.nativeElement;
+    const titleEl = document.querySelector('.date');
+    expect(titleEl?.textContent).toEqual('22/02/20');
+  });
+
+  it('excerptに"excerpt"が表示されること', () => {
+    component.excerpt = 'excerpt';
+    fixture.detectChanges();
+    const document: HTMLElement = fixture.nativeElement;
+    const excerptEl = document.querySelector('.excerpt');
+    expect(excerptEl?.textContent).toEqual('excerpt');
+  });
 });
