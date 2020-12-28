c6ef99e354a448a93298ed55bff6726f78a5398b
kozzzz-07 <kozzzz-07@noreply>
2020-12-28 12:38:36 +0900

    testing NoteContainerComponent

--- src/app/feature-modules/note/components/containers/note/note.container.spec.ts
+++ src/app/feature-modules/note/components/containers/note/note.container.spec.ts
@@ -1,4 +1,6 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoteDataService } from '../../../services/note-data.service';
 
 import { NoteContainerComponent } from './note.container';
 
@@ -9,6 +11,7 @@ describe('NoteListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [NoteContainerComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
 
@@ -20,5 +23,51 @@ describe('NoteListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('ngOnInit()の呼び出し時、Noteの取得処理が呼ばれること', () => {
+    const service = TestBed.inject(NoteDataService);
+    spyOn(service, 'fetchNotes');
+
+    component.ngOnInit();
+    expect(service.fetchNotes).toHaveBeenCalled();
+  });
+
+  it('addNote()の呼び出し時、Noteの作成処理が呼ばれること', () => {
+    const service = TestBed.inject(NoteDataService);
+    spyOn(service, 'createNote');
+    spyOn(service, 'updateNoteStatus');
+    spyOn(service, 'fetchNote');
+
+    component.addNote();
+    expect(service.createNote).toHaveBeenCalled();
+    expect(service.updateNoteStatus).toHaveBeenCalled();
+    expect(service.fetchNote).toHaveBeenCalled();
+  });
+
+  it('clickListItem()の呼び出し時、NoteStatusの更新処理が呼ばれること', () => {
+    const service = TestBed.inject(NoteDataService);
+    spyOn(service, 'updateNoteStatus');
+    spyOn(service, 'fetchNote');
+
+    component.clickListItem('1');
+    expect(service.updateNoteStatus).toHaveBeenCalled();
+    expect(service.fetchNote).toHaveBeenCalled();
+  });
+
+  it('changeTitle()の呼び出し時、NoteStatusの更新処理が呼ばれること', () => {
+    const service = TestBed.inject(NoteDataService);
+    spyOn(service, 'updateNote');
+
+    component.changeTitle('title');
+    expect(service.updateNote).toHaveBeenCalled();
+  });
+
+  it('changeDetail()の呼び出し時、NoteStatusの更新処理が呼ばれること', () => {
+    const service = TestBed.inject(NoteDataService);
+    spyOn(service, 'updateNote');
+
+    component.changeTitle('detail');
+    expect(service.updateNote).toHaveBeenCalled();
   });
 });
